85a23d71546c311e1bbf26b3c1f09f95dbeae107
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2024-11-27 14:38:03 +0100

    Ui Textfield and Button created

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,18 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                  textAlign: TextAlign.center,
+                  "HI do not think white America is committed to granting equality to the American Negro. This is a passionately racist country it will continue to be so in the foreseeable future."),
+              TextButton(onPressed: () {}, child: const Text("Text im Button")),
+            ],
+          ),
         ),
       ),
     );
