878275506ad7fc1384d315d97ffc9771cef166e8
Dirk Kraft <Dirk.kraft.akademie@gmail.com>
2024-11-27 18:48:42 +0100

    random Button generated zitate holen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,46 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import "package:http/http.dart" as http;
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+Future<List<dynamic>> getQuote() async {
+  final Uri uri = Uri.https("api.api-ninjas.com", "/v1/quotes");
+  final headers = {"X-Api-Key": "tn/NzdwIhYvvODdidg44/Q==WaiKKWqcBteD6lRP"};
+//! Hier habe ich gelernt das ich den Header auch übergeben muss bei der Get abfrage
+  final http.Response response = await http.get(uri, headers: headers);
+  if (response.statusCode == 200) {
+    final String data = response.body;
+
+    final List<dynamic> decodedJson = jsonDecode(data);
+
+    return decodedJson;
+  } else {
+    throw Exception("Fehler : Die Serverantwort war nicht 200");
+  }
+}
+
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  void getRandomQuote() async {
+    final List<dynamic> zitat = await getQuote();
+    final String randomZitat = zitat[0]["quote"];
+    String quote = "";
+    int index = 0;
+    setState(() {
+      quote = randomZitat;
+      index++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +50,30 @@ class MainApp extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const Text(
-                  textAlign: TextAlign.center,
-                  "HI do not think white America is committed to granting equality to the American Negro. This is a passionately racist country it will continue to be so in the foreseeable future."),
-              TextButton(onPressed: () {}, child: const Text("Text im Button")),
+              FutureBuilder(
+                  future: getQuote(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting)
+                      return (const CircularProgressIndicator());
+
+                    if (snapshot.hasError) {
+                      return Text(" Fehler : ${snapshot.error}");
+                    }
+                    if (snapshot.hasData) {
+                      final String randomZitat = snapshot.data![0]["quote"];
+                      return Text(
+                        randomZitat,
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontSize: 20),
+                      );
+                    }
+                    return const Text(" ist null");
+                  }),
+              TextButton(
+                  onPressed: () {
+                    getRandomQuote();
+                  },
+                  child: const Text("Text im Button")),
             ],
           ),
         ),
